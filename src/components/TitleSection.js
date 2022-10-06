3e42e9a498a36b0826f9886008ed7bd91bccebd1
Ueniry <ueniry.f@gmail.com>
2022-10-06 15:25:28 -0300

    Criada classe section no arquivo globals.css

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -3,7 +3,6 @@ import { Typography } from '@mui/material';
 const TitleSection = (props) => {
     return (
         <Typography sx={{
-            margin: "0 50px",
             fontFamily: 'Comfortaa',
             fontSize: 40,
             color: '#EC7E31'
