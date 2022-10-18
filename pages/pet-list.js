659748eaaa4430761e26d871ce20ef9851d7d4d7
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-10-17 22:41:21 -0300

    atualizacao de pagina de listagem de pets

--- pages/pet-list.js
+++ pages/pet-list.js
@@ -7,14 +7,35 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from 'next/link';
 import CreatePet from '../src/components/crud-components/CreatePet';
+import { database, onValue, child, ref } from '../src/firebase';
+import { useState, useEffect } from 'react';
 
 export default function PetList() {
+  const [pets, setPets] = useState([]);
+  useEffect(() => {
+    const petsRef = ref(database, 'pets/');
+    onValue(petsRef, (snapshot) => {
+        let snap = [];
+        snapshot.forEach((child) => {
+            var pet = child.val();
+            pet.id = child.key;
+            snap.push(pet);
+        });
+        setPets(snap);
+    });
+  }, []);
+
   return (
     <>
       <section className='section'>
         <TitleSection text='Administração dos Gatos'/>
         <CreatePet />
-        <Card />   
+        {pets.map((pet) => {
+          return (
+            <Card key={pet.id} name={pet.name} desc={pet.description} id={pet.id} pet={pet} />
+          );
+        })}
+
         
       </section>
       
