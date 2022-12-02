d48a2496bb57da13f03a3944e098abc97c0c6808
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-12-01 22:08:25 -0300

    atualizacao da pagina ver mais gatos

--- pages/gatos.js
+++ pages/gatos.js
@@ -6,13 +6,39 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from 'next/link';
+import { database, onValue, child, ref } from '../src/firebase';
+import { useState, useEffect } from 'react';
+import { useAuth } from '../src/contexts/AuthContext';
 
 export default function Home() {
+  const { currentUser } = useAuth();
+  const [pets, setPets] = useState([]);
+  useEffect(() => {
+    const petsRef = ref(database, 'pets/');
+    onValue(petsRef, (snapshot) => {
+        let snap = [];
+        snapshot.forEach((child) => {
+            var pet = child.val();
+            pet.id = child.key;
+            snap.push(pet);
+        });
+        setPets(snap);
+    });
+  }, []);
+
   return (
     <>
       <section className='section'>
         <TitleSection text='Conheça nossos gatinhos'/>
-        <Card />
+        <Grid container direction="row">
+          {pets.map((pet) => {
+            return (
+              <Grid item>
+                <Card key={pet.id} name={pet.name} desc={pet.description} id={pet.id} pet={pet} logged={currentUser} />
+              </Grid>
+            );
+          })}
+        </Grid>
         <br/>
         <Box sx={{ width: '100%' }}>
             <Grid container direction="row" justifyContent="center" alignItems="center"> 
