80ff36510afb1152e70e36f0f0dc34862d97a432
ueniry <ueniry.f@gmail.com>
2022-12-02 14:40:31 -0300

    removendo admin2 para ficar apenas admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,21 +1,22 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createRef, useState } from "react";
+import TextField from '@mui/material/TextField';
+import { useState } from "react";
 import { useAuth } from '../src/contexts/AuthContext';
 import { useRouter } from 'next/router';
+import Button from '@mui/material/Button';
+
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  input: {
+    background: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 16
+  }
+}));
 
 export default function SignIn() {
+  const classes = useStyles();
+
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const { login, currentUser, logout } = useAuth();
@@ -30,8 +31,7 @@ export default function SignIn() {
     setLoading(true);
     await login(email, password);
     router.push("/pet-list");
-
-
+ 
     setLoading(false);
   };
 
@@ -41,82 +41,92 @@ export default function SignIn() {
     setError("");
     setLoading(true);
     await logout();
-    //router.push("/");
-
 
     setLoading(false);
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+    <>
       <Box
         sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          bgcolor: "#86D5CF",
+          boxShadow: 1,
+          borderRadius: 2,
+          p: 2,
+          minWidth: 300,
+          maxWidth: 800,
+          height: 523,
+          margin: "auto",
+          marginTop: "200px",
         }}
       >
-        {JSON.stringify(currentUser)}
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            disabled={loading}
-            onClick={(e) => handleSubmit(e)}
-          >
-            Sign In
-          </Button>
+        <Typography align="center" sx={{ fontFamily: 'Comfortaa', fontSize: 42, color: '#193987', marginTop: "5%"}}>Acesso</Typography>
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            disabled={loading}
-            onClick={(e) => handleLogout(e)}
-          >
-            Logout
-          </Button>
-        </Box>
+        <form style={{ marginTop: "5%"}}>
+          <div className='div-text-field'>
+              <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              InputLabelProps={{style: {fontFamily: "Comfortaa", fontSize: 14}}}
+              sx={{ maxWidth: "593px"}}
+              InputProps={{ className: classes.input }}
+            />
+          </div>
+          <div className='div-text-field'>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              InputLabelProps={{style: {fontFamily: "Comfortaa", fontSize: 14}}}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              sx={{ maxWidth: "593px" }}
+              InputProps={{ className: classes.input }}
+            />
+          </div>
+
+          <div style={{ textAlign: "center", marginTop: 30 }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, maxWidth: 200}}
+              disabled={loading}
+              onClick={(e) => handleSubmit(e)}
+            >
+              Entrar
+            </Button>
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 1, mb: 2, maxWidth: 200}}
+                disabled={loading}
+                onClick={(e) => handleLogout(e)}
+              >
+                Sair
+              </Button>
+            </div>
+
+        </form>  
+        
       </Box>
-    </Container>
+    </>
   );
 }
